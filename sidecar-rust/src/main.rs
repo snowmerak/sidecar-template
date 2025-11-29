c637f1f715cc6130816818a7c7136e74cd94c070
snowmerak <snowmerak@outlook.com>
2025-11-30 03:17:07 +0900

    refactor(main): comment out server initialization code in main.rs

--- sidecar-rust/src/main.rs
+++ sidecar-rust/src/main.rs
@@ -1,17 +1,17 @@
-use tonic::transport::Server;
+// use tonic::transport::Server;
 // use core_proto::company_auth_v1::auth_service_server::AuthServiceServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:50052".parse()?;
+    // let addr = "0.0.0.0:50052".parse()?;
     
-    println!("Sidecar Rust Server listening on {}", addr);
+    // println!("Sidecar Rust Server listening on {}", addr);
 
-    Server::builder()
+    // Server::builder()
         // TODO: Add your services here
         // .add_service(AuthServiceServer::new(MyAuthService::default()))
-        .serve(addr)
-        .await?;
+        // .serve(addr)
+        // .await?;
 
     Ok(())
 }
